407361ce1ac5d92099fb6950166243d4083e022c
mnijaki <mnijaki@gmail.com>
2019-12-13 03:31:08 +0100

    MN:Commit info:   * Added 'Player.cs'   * Added Player to scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _playerInput = new PlayerInput();
+    }
+
+    private void Update()
+    {
+        Vector3 movementInput = new Vector3(0.0F, 0.0F, _playerInput.Vertical);
+        Vector3 movement = transform.rotation * movementInput;
+        _characterController.SimpleMove(movement);
+    }
+}
+
+public class PlayerInput
+{
+    public float Vertical
+    {
+        get { return Input.GetAxis("Vertical"); }
+    }
+}
