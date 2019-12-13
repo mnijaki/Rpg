da453df7c38afa1ff6690e616302a9516a7fd67d
mnijaki <mnijaki@gmail.com>
2019-12-13 01:12:42 +0100

    MN:Commit info:   * Renamed assembley definiotion for 'EditMode'   * Renamed test class   * Added assemble definition for 'PlayMode'   * Added tests for 'PlayMode'

--- Assets/Tests/EditMode/list_tests.cs
+++ Assets/Tests/EditMode/list_tests.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine.TestTools;
 
-namespace EditModeTests
+namespace Tests.EditMode
 {
-    public class FirstTest
+    public class list_tests
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void FirstTestSimplePasses()
+        public void string_list_test()
         {
             // Most of the times this is not appropriate checking for test (this is standard
             // .NET comparison for objects). Use 'Assert.AreEquals()' instead.
@@ -24,17 +22,7 @@ namespace EditModeTests
             strings.RemoveAt(0);
             
             // Assert.
-            Assert.AreEqual(2, strings.Count);
-        }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator FirstTestWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            Assert.AreEqual(1, strings.Count);
         }
     }
 }
---
+++ Assets/Tests/PlayMode/a_moving_cube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode
+{
+    public class a_moving_cube
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        // To see how unity test work, on MonoBehaviour switch to 'scene' tab during test.
+        [UnityTest]
+        public IEnumerator object_moving_forward_changes_position()
+        {
+            // Arrange.
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = Vector3.zero;
+            
+            for(int i = 0; i < 10; i++)
+            {
+                // Act.
+                cube.transform.position += Vector3.forward;
+                yield return new WaitForSeconds(1);
+                
+                // Assert.
+                Assert.AreEqual(i + 1, cube.transform.position.z);
+            }
+        }
+        
+        [UnityTest]
+        public IEnumerator object_is_rotated_90_degrees_in_z_axis()
+        {
+            // Arrange.
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = Vector3.zero;
+            
+            // Act.
+            cube.transform.Rotate(Vector3.forward,90);
+            
+            // Assert.
+            Assert.AreEqual(90, cube.transform.rotation.eulerAngles.z,0.1F);
+            
+            yield return null;
+        }
+    }
+}
